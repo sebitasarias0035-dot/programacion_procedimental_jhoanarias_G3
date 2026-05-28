d81380683fdbc4ffa3907340310795745c360208
jhoan Arias <sebitas.arias0035@gmail.com>
2026-05-28 17:19:40 -0500

    modify_module_usuarios : add js

---
+++ js/contabilidad.js
@@ -0,0 +1,5 @@
+let ventas =  document.getElementById("codigo_producto").ariaValueMax;
+let fecha = document.getElementById("dehoy").ariaValueMax;
+let ingresos = document.getElementById("plata").ariaValueMax;
+let egresos = document.getElementById("noplata").ariaValueMax;
+let valor = document.getElementById("osi").ariaValueMax;
